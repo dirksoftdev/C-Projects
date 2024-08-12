c6cc701cdc4d06e6f9460ba759b1823069343e57
Dirk Noyahr <dirk@Dirks-M1-MacBook-Air.local>
2024-08-11 22:16:15 -0400

    TipCalculatorV3

--- tip_calculator.c
+++ tip_calculator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// I use the round function so I have to include this library
+#include <math.h>
+
 int main(void) {
     // Declare Variables
     double billAmount,totalBill; //two decimal values for amount
@@ -10,18 +13,26 @@ int main(void) {
     printf("\nWelcome to the Tip calculator!\n\n");
 
     //if user inputs negative value , then it will ask user to input a value greater than 0
-    //it will then loop to ask for the bill amount again until the user enters a valid bill amount
+    //else if user inputs a value not equal two decimal places then it will promt to enter again
+    //it will then loop to ask for the bill amount again until the user meets the conditions
 
     do {
      
      printf("What is the bill amount?\n");
      scanf("%lf", &billAmount);
 
+     //To make sure the user only inputs a bill amount of two decimal values we will do a comparison
+     //Round function will round to the nearest two decimals and if the billAmount does not match roundedAmount it ask to enter a value with two decimals
+
+     double roundedAmount = round(billAmount * 100) / 100;
+
      if (billAmount <= 0) {
         printf("Please enter a valid bill amount greater than 0.\n");
+     } else if (billAmount != roundedAmount){
+        printf("Please enter a value with two decimal places. \n");
      }
      
-    } while (billAmount <= 0);
+    } while (billAmount <= 0 || billAmount != round(billAmount * 100) /100);
 
    
     //if user inputs negative tip percentage, it will prompt user to enter a valid tip amount
