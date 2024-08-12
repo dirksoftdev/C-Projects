ade6d5f94abddfc8ab8f64b4ae2e105124391b49
Dirk Noyahr <dirk@Dirks-M1-MacBook-Air.local>
2024-08-11 21:41:06 -0400

    TipCalculatorV2

--- tip_calculator.c
+++ tip_calculator.c
@@ -2,24 +2,67 @@
 
 int main(void) {
     // Declare Variables
-    double billamount,totalbill; //two decimal values for amount
-    int tippercentage; //a percentage 12-15%
+    double billAmount,totalBill; //two decimal values for amount
+    int tipPercentage; //a percentage 12-15%
 
 
     //Prompt User for Input
     printf("\nWelcome to the Tip calculator!\n\n");
-    printf("What is the bill amount?\n");
-    scanf("%lg", &billamount);
-    printf("What percentage are you tipping?\n");
-    scanf("%d", &tippercentage);
+
+    //if user inputs negative value , then it will ask user to input a value greater than 0
+    //it will then loop to ask for the bill amount again until the user enters a valid bill amount
+
+    do {
+     
+     printf("What is the bill amount?\n");
+     scanf("%lf", &billAmount);
+
+     if (billAmount <= 0) {
+        printf("Please enter a valid bill amount greater than 0.\n");
+     }
+     
+    } while (billAmount <= 0);
+
+   
+    //if user inputs negative tip percentage, it will prompt user to enter a valid tip amount
+    //it will then loop to ask for the tip percentage again until a valid amount is given
+
+    do {
+
+     printf("What percentage are you tipping?\n");
+     scanf("%d", &tipPercentage);
+
+     if (tipPercentage<= 0)
+     {
+        printf("Please enter a valid Tip Amount.\n");
+     }
+     
+    } while (tipPercentage<= 0);
+
+  
 
     //Calculate total bill
-    totalbill = (billamount * ((double)tippercentage/100)) + billamount;  
-    //I set tipperpercentage as double because when I kept it at int it gave me a 0 value and also for an accurate calculation
+    totalBill = (billAmount * (tipPercentage/100.0)) + billAmount;  
+    //I set it to 100.0 (double) because if I left it at 100 then tippercentage (int)/ 100(int) will give me a result of 0 everytime
 
-    //output total bill
-    printf("You owe the amount:$%.2lf\n", totalbill);
+    //output summary
+    printf("\nSummary:\n");
+    printf("Bill Amount: $%.2lf\n", billAmount);
+    // The two percent signs at the end of d is used to print the % sign - googled it
+    printf("Tip Percentage: %d%%\n", tipPercentage);
+    printf("Total Bill: $%.2lf\n", totalBill);
+
 
     return 0;
 
 }
+
+//Side Notes
+
+//%d is used in scanf to read an integer value from the user and store it in an int variable
+
+//%lf in scanf: Used to read a double value from user input.
+//%.2lf in printf: Used to print a double value with exactly two decimal places.
+
+//The do-while loop ensures that the code inside the loop runs at least once, and it will keep running until the user inputs a valid value.
+//Using if statements inside the loop, I can check the input values and provide feedback to the user if the input is invalid.
